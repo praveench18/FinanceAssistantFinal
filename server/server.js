722e0ed8c9fbeb7280f714fdd68af3cccf68dc02
Praveen <choudharypraveen1920@gmail.com>
2026-07-06 20:03:15 +0530

    phase 2

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use("/api/expense", expenseRoutes);
 app.get("/", (req, res) => {
     res.json({
         success: true,
-        message: "Finance Assistant Backend Running 🚀"
+        message: "Finance Assistant Backend Running "
     });
 });
 
@@ -31,5 +31,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(` Server running on port ${PORT}`);
 });
