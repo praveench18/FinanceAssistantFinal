44c1d03c41ccec0f8d8f5cb2c599f749b40532ee
Praveen <choudharypraveen1920@gmail.com>
2026-07-07 11:25:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         path="/add"
         element={
           <ProtectedRoute>
-            <Addexpense />
+            <AddExpense />
           </ProtectedRoute>
         }
       />
