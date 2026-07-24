b3183f69be19da1774d5c4f45475b2fcf536815f
Praveen <choudharypraveen1920@gmail.com>
2026-07-24 12:07:33 +0530

    update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,32 +1,26 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-
-transporter.verify((error) => {
-    if (error) {
-        console.log("SMTP ERROR:", error);
-    } else {
-        console.log("SMTP Ready");
-    }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, text) {
-    const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-    });
+    try {
+        const response = await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to,
+            subject,
+            text,
+        });
 
-    console.log("Mail sent:", info.messageId);
+        console.log("Mail sent:", response);
+
+        return response;
+
+    } catch (err) {
+
+        console.error("Resend Error:", err);
+        throw err;
+
+    }
 }
 
 module.exports = sendEmail;
