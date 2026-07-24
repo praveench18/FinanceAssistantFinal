2132196b00d7f0aa9a47df156c3e2b0c48e71017
Praveen <choudharypraveen1920@gmail.com>
2026-07-24 11:15:51 +0530

    api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://financeassistant-5me8.onrender.com/api"
+    baseURL: "https://financeassistant-5me8.onrender.com/api",
 });
 
 export default API;
